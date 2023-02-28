fe420ed3ed83c32874a19429ca15054c5fe163aa
yuuDev01 <pasenoaz@gmail.com>
2023-02-28 19:23:39 +0900

    todo list/ delete / all delete 추가

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,12 +1,70 @@
 <template>
-    <div>list</div>
+    <section>
+        <ul>
+            <li v-for="(todoItem,index) in todoItems" :key="todoItem" class="shadow">
+                <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                {{ todoItem }}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="far fa-trash-alt" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
+    </section>
 </template>
 
 <script>
 export default{
+    data(){
+        return {
+            todoItems:[]
+        }
+    },
+    created(){
+        if(localStorage.length>0){
+            // 로컬저장소가 비어있지 않는 경우 todoItems에 저장
+            for(var i = 0; i<localStorage.length; i++){
+                if(localStorage.key(i) != "loglevel:webpack-dev-server")
+                this.todoItems.push(localStorage.key(i));
+
+            }
+        }
+    },
+    methods:{
+        removeTodo(todoItem, index){
+            localStorage.removeItem(todoItem); //
+            this.todoItems.splice(index,1);
+        }
+    }
 
 }
 </script>
 
 <style>
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top:0;
+        text-align: left;
+    }
+    li{
+        display : flex ;
+        min-height: 50px;
+        height: 50px;
+        line-height:50px;
+        margin:0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+    
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+
+    .removeBtn{
+        margin-left: auto;
+        color: #de4343;
+    }
 </style>
